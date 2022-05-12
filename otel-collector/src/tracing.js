887349984aeeadc19c301938fe948c88e2e02cca
Dimitris Finas <dfinas@gmail.com>
2022-05-12 11:09:40 +0200

    This is v2.0.0b

--- otel-collector/src/tracing.js
+++ otel-collector/src/tracing.js
@@ -2,12 +2,12 @@
 
 // Require dependencies
 const opentelemetry = require("@opentelemetry/sdk-node");
-const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-http');
+const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-grpc');
 const { getNodeAutoInstrumentations } = require("@opentelemetry/auto-instrumentations-node");
 
 const sdk = new opentelemetry.NodeSDK({
   traceExporter: new OTLPTraceExporter({}),
-//  traceExporter: new OTLPTraceExporter({url: 'http://otel-collector:4318/v1/traces'}),
+//  traceExporter: new OTLPTraceExporter({url: 'grpc://otel-collector:4317'}),
   instrumentations: [getNodeAutoInstrumentations()]
 });
 
