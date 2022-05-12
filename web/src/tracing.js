0e6ab82159d9a07cc5a062ffb27c4ca6437a0934
Dimitris Finas <dfinas@gmail.com>
2022-05-12 10:58:01 +0200

    This is v2a

--- web/src/tracing.js
+++ web/src/tracing.js
@@ -1,8 +1,13 @@
+/* tracing.js */
+
+// Require dependencies
 const opentelemetry = require("@opentelemetry/sdk-node");
+const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-http');
 const { getNodeAutoInstrumentations } = require("@opentelemetry/auto-instrumentations-node");
 
 const sdk = new opentelemetry.NodeSDK({
-  traceExporter: new opentelemetry.tracing.ConsoleSpanExporter(),
+  traceExporter: new OTLPTraceExporter({}),
+//  traceExporter: new OTLPTraceExporter({url: 'http://otel-collector:4318/v1/traces'}),
   instrumentations: [getNodeAutoInstrumentations()]
 });
 
