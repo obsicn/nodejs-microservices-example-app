c5d176d7b896aeaf79f7a2200c654d633c2af16e
Mike Whalen <mike.whalen@lightstep.com>
2022-10-27 09:48:19 -0500

    initial commit

--- web/src/index.js
+++ web/src/index.js
@@ -1,77 +1,75 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const axios = require('axios');
+const express = require("express");
+const axios = require("axios");
 
 // Constants
 const PORT = process.env.PORT || 80;
 const HOST = process.env.HOST || "0.0.0.0";
-const SERVICE_URL = process.env.SERVICE_URL || "http://service";
+const USERS_SERVICE_URL = process.env.SERVICE_URL || "http://users";
 
 // App
 const app = express();
 
 async function checkWeather(weather, res) {
-  switch(weather) {
+  switch (weather) {
     // it's raining, we are loosing time
     case "rain":
       await sleep(1500);
-      res.status(202).send('Hello Rainy World!\n');
+      res.status(202).send("Hello Rainy World!\n");
       break;
 
     // it's snowing, generate error
     case "snow":
-      res.status(500).send('Bye Bye Snow!\n');
+      res.status(500).send("Bye Bye Snow!\n");
       console.log(`ERROR: IT IS SNOWING`);
       break;
 
     // by default, it's sunny
     default:
-      res.status(200).send('Hello Sunny World!\n');
+      res.status(200).send("Hello Sunny World!\n");
   }
 }
-
 
 async function generateWork(nb) {
   for (let i = 0; i < Number(nb); i++) {
     console.log(`*** DOING SOMETHING ${i}`);
-    // wait for 50ms to simulate some work
+    // wait for 50ms to simulate doing some work
     await sleep(50);
   }
 }
 
-
 async function main() {
-    app.get("/", (req, res) => {
-        let nbLoop = req.query.loop;
-        let weather = req.query.weather;
-        // generate some workload
-        if (nbLoop != undefined) {
-          generateWork(nbLoop);
-        }
-        // Set the response based on weather input
-        if (weather == undefined) {
-            res.send('Hello World!\n');
-        } else {
-            checkWeather(weather, res);
-        }
-    });
+  app.get("/", (req, res) => {
+    let nbLoop = req.query.loop;
+    let weather = req.query.weather;
+    // generate some work
+    if (nbLoop != undefined) {
+      generateWork(nbLoop);
+    }
+    // Set the response based on the weather input
+    if (weather == undefined) {
+      res.send("Hello World!\n");
+    } else {
+      checkWeather(weather, res);
+    }
+  });
 
-    app.get("/api/data", (req, res) => {
-        axios.get(SERVICE_URL + "/api/data")
-            .then(response => {
-                res.json(response.data);
-            })
-            .catch(err => {
-                console.error("Error forwarding request:");
-                console.error(err && err.stack || err);
-                res.sendStatus(500);
-            });
-    });
+  app.get("/api/data", (req, res) => {
+    axios
+      .get(USERS_SERVICE_URL + "/api/data")
+      .then((response) => {
+        res.json(response.data);
+      })
+      .catch((err) => {
+        console.error("Error forwarding request:");
+        console.error((err && err.stack) || err);
+        res.sendStatus(500);
+      });
+  });
 
-    await startServer();
+  await startServer();
 }
-
 
 function sleep(ms) {
   return new Promise((resolve) => {
@@ -79,24 +77,21 @@ function sleep(ms) {
   });
 }
 
-
 function startServer() {
-    return new Promise((resolve, reject) => {
-        app.listen(PORT, HOST, err => {
-            if (err) {
-                reject(err);
-            }
-            else {
-                resolve();
-            }
-        });
+  return new Promise((resolve, reject) => {
+    app.listen(PORT, HOST, (err) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
     });
+  });
 }
 
-
 main()
-    .then(() => console.log("Online"))
-    .catch(err => {
-        console.error("Failed to start!");
-        console.error(err && err.stack || err);
-    });
+  .then(() => console.log("Online"))
+  .catch((err) => {
+    console.error("Failed to start!");
+    console.error((err && err.stack) || err);
+  });
